e7922a2318adb0a83c87b2048c8bb0f93796ef0a
Ajit(JavaDeveloper) <ajitkr80923097@gmail.com>
2026-05-24 22:15:06 +0530

    my second project first commit

---
+++ BootMongoProj-03_Order_Tracking_System/src/main/java/com/ajit/BootMongoProj03OrderTrackingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.ajit;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMongoProj03OrderTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMongoProj03OrderTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ BootMongoProj-03_Order_Tracking_System/src/test/java/com/ajit/BootMongoProj03OrderTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMongoProj03OrderTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
