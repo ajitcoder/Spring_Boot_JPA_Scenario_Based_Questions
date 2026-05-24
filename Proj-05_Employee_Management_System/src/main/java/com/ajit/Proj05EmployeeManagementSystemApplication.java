96789d01e256aa48326a2da6c29c0554e4be340f
Ajit(JavaDeveloper) <ajitkr80923097@gmail.com>
2026-05-24 22:20:26 +0530

    initial commit

---
+++ Proj-05_Employee_Management_System/src/main/java/com/ajit/Proj05EmployeeManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.ajit;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj05EmployeeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj05EmployeeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Proj-05_Employee_Management_System/src/test/java/com/ajit/Proj05EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj05EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
