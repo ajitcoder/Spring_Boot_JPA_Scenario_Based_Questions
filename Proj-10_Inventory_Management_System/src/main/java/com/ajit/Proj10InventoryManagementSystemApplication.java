a9bad8ffc84efb3618a0bf55518391b872398bd2
Ajit(JavaDeveloper) <ajitkr80923097@gmail.com>
2026-05-24 22:23:48 +0530

    initial commit

---
+++ Proj-10_Inventory_Management_System/src/main/java/com/ajit/Proj10InventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ajit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj10InventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj10InventoryManagementSystemApplication.class, args);
+	}
+
+}
