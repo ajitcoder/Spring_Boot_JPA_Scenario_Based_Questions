8ebc48464d8c1b8dd45184eaa12e2b7da3e8926f
Ajit(JavaDeveloper) <ajitkr80923097@gmail.com>
2026-05-24 22:23:15 +0530

    initial commit

---
+++ Proj-09_Online_Course_Management_System/src/main/java/com/ajit/Proj09OnlineCourseManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.ajit;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj09OnlineCourseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj09OnlineCourseManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Proj-09_Online_Course_Management_System/src/test/java/com/ajit/Proj09OnlineCourseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj09OnlineCourseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
