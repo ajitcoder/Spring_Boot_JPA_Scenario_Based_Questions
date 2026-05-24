fadc250245919ab1d7f52c1983f9637e43df6a26
Ajit(JavaDeveloper) <ajitkr80923097@gmail.com>
2026-05-24 22:12:33 +0530

    initial commit

---
+++ BootMongoProj-02_Product_Inventory_System/src/main/java/com/ajit/BootMongoProj02ProductInventorySystemApplication.java
@@ -0,0 +1,14 @@
+package com.ajit;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMongoProj02ProductInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMongoProj02ProductInventorySystemApplication.class, args);
+	}
+
+}
